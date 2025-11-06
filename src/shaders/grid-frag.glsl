e7467cc3a4dd35e19cde350b9b0006402f311149
rubenaryo <rubeny484@gmail.com>
2025-11-06 12:30:01 -0500

    Anti aliased grid lines
    I spent too much time on this

--- src/shaders/grid-frag.glsl
+++ src/shaders/grid-frag.glsl
@@ -10,12 +10,17 @@ uniform float u_LineWidth;
 out vec4 fragColor;
 
 void main() {
-    // Compute distance from nearest grid line along X and Z, relative to origin
-    float fx = abs(mod(fs_Pos.x, u_GridSpacing));
-    float fz = abs(mod(fs_Pos.z, u_GridSpacing));
+    // distance to nearest line along X and Z
+    float fx = mod(abs(fs_Pos.x), u_GridSpacing);
+    float fz = mod(abs(fs_Pos.z), u_GridSpacing);
 
-    float line = step(fx, u_LineWidth) + step(fz, u_LineWidth);
-    line = clamp(line, 0.0, 1.0);
+    float dx = fwidth(fs_Pos.x);
+    float dz = fwidth(fs_Pos.z);
 
+    // anti-aliased line intensity
+    float lineX = 1.0 - smoothstep(0.0, u_LineWidth + dx, fx);
+    float lineZ = 1.0 - smoothstep(0.0, u_LineWidth + dz, fz);
+
+    float line = clamp(lineX + lineZ, 0.0, 1.0);
     fragColor = vec4(u_GridColor, line);
 }
