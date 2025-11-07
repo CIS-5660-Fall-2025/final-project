00cf9547952c66b262777e974a5d6894090d8522
rubenaryo <rubeny484@gmail.com>
2025-11-07 01:09:03 -0500

    Create even hex grid, with axial coordinate spacing

--- src/SceneManager.ts
+++ src/SceneManager.ts
@@ -6,20 +6,21 @@ import Camera from './Camera';
 export class SceneManager {
     hex: Hex;
     tiles: vec2[];
-    tilesWidth: number;
-    tilesHeight: number;
+    tilesRadius: number;
 
     loadScene(): void {
         this.hex = new Hex();
         this.hex.create();
 
-        this.tilesWidth = 10;
-        this.tilesHeight = 10;
+        this.tilesRadius = 5;
 
         this.tiles = [];
-        for (let i = 0; i != this.tilesWidth; ++i) {
-            for (let j = 0; j != this.tilesHeight; ++j) {
-                this.tiles.push(vec2.fromValues(i, j));
+        for (let q = -this.tilesRadius; q <= this.tilesRadius; ++q) {
+            for (let r = -this.tilesRadius; r <= this.tilesRadius; ++r) {
+                for (let s = -this.tilesRadius; s <= this.tilesRadius; ++s) {
+                    if ((q + r + s) == 0)
+                        this.tiles.push(vec2.fromValues(q, r));
+                }
             }
         }
     }
@@ -35,19 +36,22 @@ export class SceneManager {
         mat4.multiply(viewProj, camera.projectionMatrix, camera.viewMatrix);
         prog.setUniformMat4("u_ViewProj", viewProj);
         prog.setUniformVec4("u_Color", color);
-        
-        for (let i = 0; i != this.tilesWidth; ++i) {
-            for (let j = 0; j != this.tilesHeight; ++j) {
-                
-                mat4.fromTranslation(model, vec3.fromValues(i*2, 0, j*2));
-                prog.setUniformMat4("u_Model", model);
-                mat4.transpose(modelinvtr, model);
-                mat4.invert(modelinvtr, modelinvtr);
-                prog.setUniformMat4("u_ModelInvTr", modelinvtr);
 
-                prog.draw(this.hex);
+        this.tiles.forEach((tile) => {
+            const q = tile[0];
+            const r = tile[1];
 
-            }
-        }
+            var x = (3. / 2 * q);
+            var y = (Math.sqrt(3) / 2 * q + Math.sqrt(3) * r);
+
+            let v = vec3.fromValues(x * 1.1, 0, y * 1.1);
+
+            mat4.fromTranslation(model, v);
+            prog.setUniformMat4("u_Model", model);
+            mat4.transpose(modelinvtr, model);
+            mat4.invert(modelinvtr, modelinvtr);
+            prog.setUniformMat4("u_ModelInvTr", modelinvtr);
+            prog.draw(this.hex);
+        });
     }
 }
